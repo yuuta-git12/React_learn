21dc2b308788b31f3def5a171c85ee5718822b9c
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2026-01-02 22:46:23 +0900

    [feature0003/react_jsx_tsx]-[update]：ViteにMsgコンポーネント追加

--- apps/react-vite/src/App.tsx
+++ apps/react-vite/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState<number>(0)
+const title = "React page.";
+const message = "メッセージを表示します";
 
+function Msg(){
+  return <p className='msg'>Hello!!</p>
+}
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((value) => value + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='container'>
+      <h1>{title}</h1>
+      <h2>{message}</h2>
+      <Msg />
+      <Msg />
+      <Msg />
+    </div>
+  );
 }
 
 export default App
