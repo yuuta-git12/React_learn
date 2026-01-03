1a9c6001851496ebbf7c473a3683db731d52a9a9
yuuta-git12 <42604764+yuuta-git12@users.noreply.github.com>
2026-01-03 16:44:26 +0900

    [feature0003/react_jsx_tsx]-[update]：JSX(TSX)ファイルでのコンポーネント、インターフェースの定義・使い方、スタイルの適用方法について

--- apps/react-cra/src/App.tsx
+++ apps/react-cra/src/App.tsx
@@ -5,18 +5,59 @@ import './App.css';
 const title = "React page.";
 const message = "メッセージを表示します";
 
-function Msg(){
-  return <p className='msg'>Hello!!</p>
+function Msg(msg:string, size:number, color:string){
+  const s = {
+    fontSize: size + "pt",
+    color: color,
+  }
+  return <p className='msg' style={s}>{msg}</p>
+}
+
+// Msg2Propsインターフェースの定義
+interface Msg2Props {
+  msg: string,
+  size: number,
+  color: string
+}
+
+function Msg2(props: Msg2Props){
+  const s = {
+    fontSize: props.size + "pt",
+    color: props.color,
+  }
+  return <p className='msg' style={s}>{props.msg}</p>
 }
 
 function App() {
   return (
     <div className='container'>
+      {/* コンポーネント内のスタイル設定 */}
+      <style>{`
+        h1 {
+          coloer: white;
+          background-color: blue;
+          padding: 5px;
+        }
+        h2 {
+          color: white;
+          background-color: red;
+          padding: 5px 10px;
+        }
+        p.msg {
+          background-color: lightyellow;
+        }
+      `}</style>
       <h1>{title}</h1>
       <h2>{message}</h2>
-      <Msg />
-      <Msg />
-      <Msg />
+      {/* Msgに引数を渡して呼び出し */}
+      { Msg("最初のメッセージ", 36, "red")}
+      { Msg("次のメッセージ", 24, "lightgray")}
+      { Msg("最後のメッセージ", 12, "black")}
+
+      {/* Msg2のprops(属性)を使って値を渡す場合 */}
+      <Msg2 msg={"最初のメッセージ2"} size={20} color={"blue"} />
+      <Msg2 msg={"次のメッセージ2"} size={20} color={"green"} />
+      <Msg2 msg={"最後のメッセージ2"} size={20} color={"orange"} />
     </div>
   );
 }
--- apps/react-vite/src/App.tsx
+++ apps/react-vite/src/App.tsx
@@ -3,8 +3,28 @@ import './App.css'
 const title = "React page.";
 const message = "メッセージを表示します";
 
-function Msg(){
-  return <p className='msg'>Hello!!</p>
+function Msg(msg:string, size:number, color:string){
+  const s = {
+    fontSize: size + "pt",
+    color: color,
+    fontWeight: "bold",  
+  }
+  return <p className='msg' style={s}>{msg}</p>
+}
+
+// Msg2Propsインターフェースの定義
+interface Msg2Props {
+  msg: string,
+  size: number,
+  color: string
+}
+
+function Msg2(props: Msg2Props){
+  const s = {
+    fontSize: props.size + "pt",
+    color: props.color,
+  }
+  return <p className='msg' style={s}>{props.msg}</p>
 }
 
 function App() {
@@ -12,9 +32,15 @@ function App() {
     <div className='container'>
       <h1>{title}</h1>
       <h2>{message}</h2>
-      <Msg />
-      <Msg />
-      <Msg />
+      {/* Msgに引数を渡して呼び出し */}
+      { Msg("最初のメッセージ", 36, "red")}
+      { Msg("次のメッセージ", 24, "lightgray")}
+      { Msg("最後のメッセージ", 12, "black")}
+
+      {/* Msg2のprops(属性)を使って値を渡す場合 */}
+      <Msg2 msg={"最初のメッセージ2"} size={20} color={"blue"} />
+      <Msg2 msg={"次のメッセージ2"} size={20} color={"green"} />
+      <Msg2 msg={"最後のメッセージ2"} size={20} color={"orange"} />
     </div>
   );
 }
